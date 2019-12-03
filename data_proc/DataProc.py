0ceadcff4fc372e5a0082bc40147e833ddb87e2e
LixiangRu <rulixiang@outlook.com>
2019-12-03 13:43:27 +0800

    update data processing code

--- data_proc/DataProc.py
+++ data_proc/DataProc.py
@@ -7,9 +7,10 @@ Created on Fri Jul 26 16:07:46 2019
 
 import os
 import re
-
+import shutil
 import numpy as np
 from matplotlib import pyplot as plt
+import sklearn.metrics as metrics
 import matplotlib.image as mpimg
 
 from config import config
@@ -56,12 +57,50 @@ def genFileList(label_map_t1, label_map_t2, cfg=None):
     idx_col = index[1]
     label_t1 = label_map_t1[idx_row, idx_col].astype(np.int16)
     label_t2 = label_map_t2[idx_row, idx_col].astype(np.int16)
+    cm = metrics.confusion_matrix(label_t1, label_t2)
+    np.set_printoptions(precision=3, suppress=True)
+    #print(cm)
+    num = np.sum(label_t1!=label_t2)
+
     filename_t1 = ['']*len(idx_col)
     filename_t2 = ['']*len(idx_col)
+    #cnt=1    
     for k in range(len(idx_col)):
         filename_t1[k] = cfg.dir_t1+cfg.class_name[label_t1[k]]+'/'+str(idx_col[k]+1)+'_'+str(idx_row[k]+1)+'.png'
         filename_t2[k] = cfg.dir_t2+cfg.class_name[label_t2[k]]+'/'+str(idx_col[k]+1)+'_'+str(idx_row[k]+1)+'.png'
-
+        '''
+        if label_t1[k]!=label_t2[k]:
+            img_t1 = mpimg.imread(filename_t1[k])
+            img_t2 = mpimg.imread(filename_t2[k])
+            
+            plt.close()
+            plt.subplot('121')
+            plt.imshow(img_t1)
+            plt.title(cfg.class_name[label_t1[k]])
+            plt.subplot('122')
+            plt.imshow(img_t2)
+            plt.title(cfg.class_name[label_t2[k]])
+            plt.show()
+            
+            a=input('input right class：')
+            
+            if a is '1':
+                filename = cfg.dir_t2+cfg.class_name[label_t2[k]]+'/'+str(idx_col[k]+1)+'_'+str(idx_row[k]+1)+'.png'
+                new_filename = cfg.dir_t2+cfg.class_name[label_t1[k]]+'/'+str(idx_col[k]+1)+'_'+str(idx_row[k]+1)+'.png'
+                print('Moving %s to %s\n'%(filename, new_filename))
+                shutil.move(filename, new_filename)
+            elif a is '2':
+                filename = cfg.dir_t1+cfg.class_name[label_t1[k]]+'/'+str(idx_col[k]+1)+'_'+str(idx_row[k]+1)+'.png'
+                new_filename = cfg.dir_t1+cfg.class_name[label_t2[k]]+'/'+str(idx_col[k]+1)+'_'+str(idx_row[k]+1)+'.png'
+                print('Moving %s to %s\n'%(filename, new_filename))
+                shutil.move(filename, new_filename)
+            else:
+                print('No operation between %s and %s\n'%(filename_t1[k],filename_t2[k]))
+            
+            #plt.show()
+            print('%3d / %3d\n\n'%(cnt, num))
+            cnt = cnt+1
+        '''
 
     return filename_t1, filename_t2, label_t1, label_t2
 
@@ -106,17 +145,65 @@ def main(cfg=None):
     #plt.imshow(colormap_t1)
     #plt.show()
     
-    new_labelmap_t1 = np.zeros_like(labelmap_t1)
-    new_labelmap_t2 = np.zeros_like(labelmap_t2)
+    new_label_map_t1 = np.zeros_like(labelmap_t1)
+    new_label_map_t2 = np.zeros_like(labelmap_t2)
                 
-    new_color_map_t1, new_labelmap_t1 = genNolap(colormap_t1, labelmap_t1, cfg=cfg)
-    new_color_map_t2, new_labelmap_t2 = genNolap(colormap_t2, labelmap_t2, cfg=cfg)
+    new_color_map_t1, new_label_map_t1 = genNolap(colormap_t1, labelmap_t1, cfg=cfg)
+    new_color_map_t2, new_label_map_t2 = genNolap(colormap_t2, labelmap_t2, cfg=cfg)
+    
+    for row in range(np.shape(new_label_map_t1)[0]):
+        for col in range(np.shape(new_label_map_t1)[1]):
+            lt1 = int(new_label_map_t1[row,col])
+            lt2 = int(new_label_map_t2[row,col])
+            if lt1==0 or lt2==0:
+                new_label_map_t1[row,col] = 0
+                new_label_map_t2[row,col] = 0
+                new_color_map_t1[row,col,:] = 0
+                new_color_map_t2[row,col,:] = 0
 
-    plt.imsave('label_t1.png',new_color_map_t1)
-    plt.imsave('label_t2.png',new_color_map_t2)
+                #a = plt.imread(filename_t1)
+                #plt.imsave(filename_t2, a)
+    
+    new_color_map_t1 = np.repeat(new_color_map_t1, 10, axis=0)
+    new_color_map_t2 = np.repeat(new_color_map_t2, 10, axis=0)
+    new_color_map_t1 = np.repeat(new_color_map_t1, 10, axis=1)
+    new_color_map_t2 = np.repeat(new_color_map_t2, 10, axis=1)
 
+    print(np.sum(new_label_map_t1!=0))
+    print(np.sum(new_label_map_t1!=new_label_map_t2))
+    
+    plt.imsave('co_mask_t1.png',new_color_map_t1)
+    plt.imsave('co_mask_t2.png',new_color_map_t2)
+    
+    #os.mkdir('2014')
+    #os.mkdir('2014/semisure')
+    #os.mkdir('2016')
+    #os.mkdir('2016/semisure')
+    '''
+    for ndir in cfg.class_name:
+        os.mkdir('2014/semisure/'+ndir)
+        os.mkdir('2016/semisure/'+ndir)
+    import shutil
+    for row in range(np.shape(new_label_map_t1)[0]):
+        for col in range(np.shape(new_label_map_t1)[1]):
+            lt1 = int(new_label_map_t1[row,col])
+            lt2 = int(new_label_map_t2[row,col])
+            if lt1!=0 and lt2==0:
+                filename_t1 = '2016/notsure/'+str(col+1)+'_'+str(row+1)+'.png'
+                filename_t2 = '2016/semisure/'+cfg.class_name[lt1]+'/'+str(col+1)+'_'+str(row+1)+'.png'
+                shutil.move(filename_t1,filename_t2)
+                #a = plt.imread(filename_t1)
+                #plt.imsave(filename_t2, a)
 
-    filename_t1, filename_t2, label_t1, label_t2 = genFileList(new_labelmap_t1, new_labelmap_t2, cfg=cfg)
+            elif lt1==0 and lt2!=0:
+                filename_t1 = '2014/semisure/'+cfg.class_name[lt2]+'/'+str(col+1)+'_'+str(row+1)+'.png'
+                filename_t2 = '2014/notsure/'+str(col+1)+'_'+str(row+1)+'.png'
+                shutil.move(filename_t2,filename_t1)
+                #a = plt.imread(filename_t2)
+                #plt.imsave(filename_t1, a)
+    '''
+
+    filename_t1, filename_t2, label_t1, label_t2 = genFileList(new_label_map_t1, new_label_map_t2, cfg=cfg)
 
     trn_rate = 0.7
     tst_rate = 0.2
@@ -138,12 +225,13 @@ def main(cfg=None):
         'label_t2': label_t2
     }
 
-    saveNpy(out_dir='./data_small/trn/',file=file_dict, index=trn_idx)
-    saveNpy(out_dir='./data_small/val/',file=file_dict, index=val_idx)
-    saveNpy(out_dir='./data_small/tst/',file=file_dict, index=tst_idx)
+    #saveNpy(out_dir='./data_small/trn/',file=file_dict, index=trn_idx)
+    #saveNpy(out_dir='./data_small/val/',file=file_dict, index=val_idx)
+    #saveNpy(out_dir='./data_small/tst/',file=file_dict, index=tst_idx)
 
     return True
 
 if __name__ == "__main__":
     cfg = config()
+
     main(cfg=cfg)
--- data_proc/config.py
+++ data_proc/config.py
@@ -3,10 +3,7 @@ import numpy as np
 
 class config():
     def __init__(self):
-        arr = [[128,   0,   0],[128, 128,   0],[  0,   0, 128],[  0, 128,   0],
-               [  0, 192,   0],[128, 128, 128],[192, 128,   0],[ 64,   0, 128],
-               [128,   0, 128],[192,   0, 128],[128,  64,   0],[192, 128, 128],
-               [ 64,  64,   0],[  0,  64, 128],]
+        arr = np.array([[0,0,205],[65,105,225],[135,206,235],[0,139,69],[0,216,0],[238,154,73],[163,124,2],[255,38,38],[205,38,38],[139,26,26],[255,231,186],[48,48,48],[179,151,143],[186,85,211]],dtype=np.float32)/255  
         self.colormap = np.array(arr,dtype=np.float32) / np.max(arr)
         self.class_name = ['0-undefined',
             '1-administration', '2-commercial', '3-water', '4-farmland',
@@ -15,6 +12,6 @@ class config():
             '11-road', '12-parking', '13-bareland', '14-playground'
         ]
         self.shape = [190, 237]
-        self.dir_t1 = 'D:/lib/wuhan/img_process/2014/sure/'
-        self.dir_t2 = 'D:/lib/wuhan/img_process/2016/sure/'
+        self.dir_t1 = './2014/sure/'
+        self.dir_t2 = './2016/sure/'
         self.step = 1
